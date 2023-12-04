391c476253be8ca3b53adc5e382835c34f967593
reactnenja <reactnenja@gmail.com>
2023-12-04 23:02:40 +0500

    tayyor

---
+++ assets/js/min.js
@@ -0,0 +1,38 @@
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+    navbar.classList.remove("navbar-shrink");
+  }
+}
+
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.classList.add("backtop-show");
+  } else {
+    backtop.classList.remove("backtop-show");
+  }
+}
+
+const loading = document.getElementById("loading");
+
+const loadingDuration = 2500; // 4s
+
+setTimeout(() => {
+  loading.classList.add("loading-none");
+}, loadingDuration);
